ccb423f9dced1897518e591a040e00a3f0805c5c
fcxmarquez <francisco.marquez.solt@gmail.com>
2024-08-20 17:22:00 -0600

    Added footer

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -18,45 +18,50 @@ export const Home = () => {
 
   return (
     <>
-      <Box minHeight="100vh" bg="gray.900" color="white" p={8} display="flex" justifyContent="center" alignItems="center">
-        <VStack spacing={6} maxWidth="400px">
-          <Heading>Whisper Transcript</Heading>
-          <Box width="100%">
-            <input
-              type="file"
-              accept="audio/*"
-              onChange={handleFileChange}
-              style={{ display: 'none' }}
-              id="file-input"
-            />
-            <label htmlFor="file-input">
-              <Button as="span" colorScheme="blue" width="100%">
-                Choose File
-              </Button>
-            </label>
-            <Text mt={2} fontSize="sm">
-              {file ? file.name : 'No file chosen'}
-            </Text>
-          </Box>
-          <Button
-            colorScheme="blue"
-            width="100%"
-            onClick={handleTranscribe}
-            isDisabled={!file}
-          >
-            Upload & Transcribe
-          </Button>
-          <VStack width="100%" align="start">
-            <Text fontWeight="bold">Transcription:</Text>
-            <Textarea
-              value={transcription}
-              readOnly
-              bg="gray.700"
-              borderColor="gray.600"
-              height="100px"
-            />
+      <Box minHeight="100vh" bg="gray.900" color="white" p={8} display="flex" flexDirection="column" justifyContent="space-between">
+        <Box display="flex" justifyContent="center" alignItems="center" flex="1">
+          <VStack spacing={6} maxWidth="400px">
+            <Heading>Whisper Transcript</Heading>
+            <Box width="100%">
+              <input
+                type="file"
+                accept="audio/*"
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+                id="file-input"
+              />
+              <label htmlFor="file-input">
+                <Button as="span" colorScheme="blue" width="100%">
+                  Choose File
+                </Button>
+              </label>
+              <Text mt={2} fontSize="sm">
+                {file ? file.name : 'No file chosen'}
+              </Text>
+            </Box>
+            <Button
+              colorScheme="blue"
+              width="100%"
+              onClick={handleTranscribe}
+              isDisabled={!file}
+            >
+              Upload & Transcribe
+            </Button>
+            <VStack width="100%" align="start">
+              <Text fontWeight="bold">Transcription:</Text>
+              <Textarea
+                value={transcription}
+                readOnly
+                bg="gray.700"
+                borderColor="gray.600"
+                height="100px"
+              />
+            </VStack>
           </VStack>
-        </VStack>
+        </Box>
+        <Text textAlign="center" fontSize="sm" mt={4}>
+          Made by <Text as="span" fontWeight="bold">fcxmarquez</Text>
+        </Text>
       </Box>
     </>
   )
