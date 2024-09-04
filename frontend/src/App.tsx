53bd75283ee2f61fde752b3f7e42b149378f05e1
fcxmarquez <francisco.marquez.solt@gmail.com>
2024-09-04 17:15:40 -0600

    config react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
-import { Home } from '@/views/Home'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Home } from './views/Home'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Home />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Home />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
